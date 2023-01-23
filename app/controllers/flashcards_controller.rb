3ee6ba8da52c6899036f8baace409f0e81d4763c
mizokawa <guitarmizo1350@gmail.com>
2023-01-23 19:04:06 +0900

    new create機能の作成

---
+++ app/controllers/flashcards_controller.rb
@@ -0,0 +1,36 @@
+class FlashcardsController < ApplicationController
+  #問題の一覧表示
+  def index
+
+  end
+
+  #問題解答ページ表示
+  def show
+
+  end
+
+  #問題の作成
+  def new
+
+  end
+
+  #問題の登録
+  def create
+
+  end
+
+  #問題の編集
+  def edit
+
+  end
+
+  #問題の更新
+  def update
+
+  end
+
+  #問題の削除
+  def destroy
+    
+  end
+end
