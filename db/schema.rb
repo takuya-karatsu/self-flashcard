8c4d2b2d342306cca24e186529af40348daedbf4
mizokawa <guitarmizo1350@gmail.com>
2023-01-28 21:55:08 +0900

    learningのページ実装およびCSSを用いて解答の表示実装

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
   create_table "flashcards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "category"
@@ -6,6 +5,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
     t.text "answer"
     t.text "description"
     t.string "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
