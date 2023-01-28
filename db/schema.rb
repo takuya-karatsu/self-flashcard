7fa5e38c6d238989d8af4a898ed0f5199fa93f52
mizokawa <guitarmizo1350@gmail.com>
2023-01-28 21:22:39 +0900

    bootstrapの取り込み、dbスキーマ競合していたのでusersのdb情報は削除した。

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
   create_table "flashcards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "category"
