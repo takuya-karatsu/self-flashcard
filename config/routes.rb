c47f0d670dbc101cdb390c82be634b9bd06f5e60
mizokawa <guitarmizo1350@gmail.com>
2023-01-30 22:53:20 +0900

    訂正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "flashcards#index"
+
   resources :flashcards
-  get "/learning", to: "flashcards#learning"
+    get "/learning", to: "flashcards#learning"
 end
