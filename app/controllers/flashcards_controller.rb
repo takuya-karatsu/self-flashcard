bcaee2ea69a3adf2d0d372458fbf033de8beda38
koki037 <guitarmizo1350@gmail.com>
2023-02-01 19:19:26 +0900

    コミットの復元

---
+++ app/controllers/flashcards_controller.rb
@@ -0,0 +1,64 @@
+class FlashcardsController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  #問題の一覧表示
+  def index
+    @flashcards = Flashcard.all
+
+  end
+
+  #問題解答ページ表示
+  def show
+  end
+
+  #問題の作成
+  def new
+    @flashcard = Flashcard.new
+  end
+
+  #問題の登録
+  def create
+    #Flashcardモデルを初期化
+    @flashcard = Flashcard.new(flashcard_params)
+    #FlashcardモデルをDBへ保存
+    if @flashcard.save
+    #showへリダイレクト
+      redirect_to @flashcard
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  #問題の編集
+  def edit
+  end
+
+  #問題の更新
+  def update
+    if @flashcard.update(flashcard_params)
+      redirect_to @flashcard
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  #問題の削除
+  def destroy
+    @flashcard.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  #flashcardでの学習
+  def learning
+    quiz = rand(Flashcard.first.id..Flashcard.last.id)
+    @flashcard = Flashcard.where('id >=?', quiz).first
+  end
+
+  private
+  def flashcard_params
+    params.require(:flashcard).permit(:category, :question, :answer, :description)
+  end
+
+  def set_flashcard
+    @flashcard = Flashcard.find(params[:id])
+  end
+end
