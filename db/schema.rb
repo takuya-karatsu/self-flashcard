ec7e023bda3f8498f7f42d0946b5a8ae2ea8fb2d
takuya-karatsu <ktakuku14@gmail.com>
2023-02-05 13:57:36 +0900

    スキーマ修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,5 @@
 ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
-  create_table "flashcards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "category"
-    t.text "question"
-    t.text "answer"
-    t.text "description"
-    t.string "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
-  create_table "flashcards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "flashcards", charset: "utf8", force: :cascade do |t|
     t.string "category"
     t.text "question"
     t.text "answer"
@@ -22,7 +9,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_094456) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name"
