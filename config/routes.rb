c0bde068b16c3f9017dd58cfdcf4e3fc35e1803d
takuya-karatsu <ktakuku14@gmail.com>
2023-02-15 00:08:30 +0900

    検索のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :flashcards
-    get "/learning", to: "flashcards#learning"
+  resources :flashcards do
+    collection do
+      get 'search'
+    end
+  end
+  get "/learning", to: "flashcards#learning"
 end
