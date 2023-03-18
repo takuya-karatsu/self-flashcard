fb904b1411c1984075bb02ce8d8a2eeed443e1f3
koki037 <guitarmizo1350@gmail.com>
2023-03-18 10:45:18 +0900

    レッスンでの変更、bootstrap5へ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+import Popper from 'popper.js';
+import Turbolinks from 'turbolinks'
+Turbolinks.start();
